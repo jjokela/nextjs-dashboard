e97e32ec8f5a0248b916564f4f96f810f8ed60e9
jjokela <jarmo.jokela@gmail.com>
2024-06-22 10:10:58 +0200

    wip

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,11 +21,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 1 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -38,6 +38,9 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
